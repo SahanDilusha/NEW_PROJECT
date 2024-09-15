9cb38315e08895ab9670a394cc7988d6ed5e60f0
SahanDilusha <sahancodelygroup@gmail.com>
2024-09-16 00:09:52 +0530

    update

--- build/web/my_js/shop.js
+++ build/web/my_js/shop.js
@@ -1,11 +1,14 @@
 async  function getFilterData() {
     
     const response = await fetch("RemoveCartItem", {
-        headers: {"Content-Type": "application/json"},
+        headers: {"Content-Type": "application/json"}
     });
 
     if (response.ok) {
         
+        const json = await response.json();
+        
+        console.log(json);
         
     }
     
